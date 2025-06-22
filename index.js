9ca8a2ff9bed92bea80f06fb70d6c4b1d15fedd7
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-06-22 19:12:00 +0300

    enters the latest commits

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    return title
+      .split(' ')
+      .map(word => {
+        if (word === word.toUpperCase()) return word; // handle acronyms like JSONP
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(' ');
+  });
 }
